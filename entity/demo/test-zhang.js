5a01d59384fa34c6e00ff7a6dd2466282eee1eee
zhangmingfeng <zhangmf2008@126.com>
2018-10-08 18:48:30 +0800

    表结构对比

--- entity/demo/test-zhang.js
+++ entity/demo/test-zhang.js
@@ -4,13 +4,15 @@ module.exports = {
         attr1: {
             type: 'integer',
             length: 11,
-            precision: 10,
-            scale: 2,
             allowNull: false,
             defaultValue: 0,
-            primaryKey: true,
-            autoIncrement: true,
             comment: '地址ID'
+        },
+        attr6: {
+            type: 'decimal',
+            allowNull: false,
+            defaultValue: 0.01,
+            comment: '测试attr6attr6'
         }
     }
 };
